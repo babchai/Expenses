aa68fee9087ed5ff51c66f3ff81cbc1ef843f393
sai chang choon <babchai@hotmail>
2015-04-07 01:56:15 +0700

    first commit.

---
+++ www/js/controllers.js
@@ -0,0 +1,257 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http, $rootScope, $localstorage, $state) {
+
+
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+	var hour = today.getHours();
+	var minute = today.getMinutes();
+	
+	$scope.category = "";
+	$scope.expenses={};
+
+
+	$http.get('./models/category.json')
+       .then(function(res){
+       	//console.log(res);
+          $scope.categories = chunk(res.data, 5);   
+        });
+
+   var settings = $localstorage.get('settings');     
+
+    if(typeof settings != "undefined"){
+      settings = JSON.parse(settings);
+      $scope.expenses.currency = settings.currency.label;
+    }
+    else
+    { 
+       $http.get('./models/settings.json')
+       .then(function(res){
+           console.log(res);
+          $scope.expenses.currency = res.data.default.currency;
+        });
+    }
+
+
+   $scope.expenses.date = today;
+    
+  
+    function chunk(arr, size) {
+	  var newArr = [];
+	  for (var i=0; i<arr.length; i+=size) {
+	    newArr.push(arr.slice(i, i+size));
+	  }
+	  return newArr;
+	}
+   
+    $scope.selected=function( $label, $event){
+    	$elemet =  angular.element($event.currentTarget);
+    	if($elemet.parent().hasClass('selected')){
+    		$elemet.parent().removeClass('selected');
+    		$scope.expenses.category = "";
+
+
+    	}
+    	else
+    	{
+   			$e = angular.element(document.getElementsByClassName('selected'));
+			$e.removeClass('selected');
+    		$elemet.parent().addClass('selected');
+    		//console.log($label);
+    		$scope.expenses.category = $label;
+
+
+    	} 
+
+    }
+
+    $scope.save = function(){
+
+
+    	console.log(this.expenses);
+      var date = new Date(this.expenses.date);
+      var dd = date.getDate();
+      var mm = date.getMonth()+1; //January is 0!
+      var yyyy = date.getFullYear();
+
+      this.expenses.date = yyyy+"-"+mm+"-"+dd;
+	
+    	var data = $localstorage.get('expenses');
+    	if(typeof data != 'undefined' ){
+    		console.log(data);
+
+    		data = JSON.parse(data);
+    	}
+    	else
+    	{
+    		data = [];
+    	}
+
+        data.push(this.expenses);
+        $localstorage.set('expenses', JSON.stringify(data));
+
+        $state.go('tab.chats', {}, {reload: true});
+
+
+    }
+
+    $scope.getPhoto = function() {
+	    navigator.camera.getPicture(onSuccess, onFail, { quality: 75, targetWidth: 320,
+	    targetHeight: 320, destinationType: 0 }); 
+	    //destination type was a base64 encoding
+	    function onSuccess(imageData) {
+	    	//console.log(imageData);
+	        //preview image on img tag
+	       // $('#image-preview').attr('src', "data:image/jpeg;base64,"+imageData);
+
+	       $scope.preview_image = "data:image/jpeg;base64,"+imageData;
+	        //setting scope.lastPhoto 
+	        //console.log(imageData);
+            $scope.expenses.photo =  "data:image/jpeg;base64,"+imageData;
+	       // $scope.expenses.photo = dataURItoBlob("data:image/jpeg;base64,"+imageData);
+
+	        $scope.$apply();
+	    }
+	    function onFail(message) {
+	        alert('Failed because: ' + message);
+	    }
+	} 
+	function dataURItoBlob(dataURI) {
+		// convert base64/URLEncoded data component to raw binary data held in a string
+		 var byteString = atob(dataURI.split(',')[1]);
+		 var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+		 var ab = new ArrayBuffer(byteString.length);
+		 var ia = new Uint8Array(ab);
+		 for (var i = 0; i < byteString.length; i++)
+		 {
+		    ia[i] = byteString.charCodeAt(i);
+		 }
+
+		 var bb = new Blob([ab], { "type": mimeString });
+		 return bb;
+	}
+})
+
+.controller('ChatsCtrl', function($scope, Chats, $localstorage, $http, $filter, Categories, _) {
+    
+    var expenses = JSON.parse($localstorage.get('expenses'));
+     
+    $scope.expenses = expenses;
+
+    $scope.datasets = _.groupBy(expenses, 'date');
+
+    $http.get('./models/currencies.json')
+         .then(function(res){
+            for(var j=0 ; j<res.data.length; j++)
+            {
+              if(res.data[j].label == "MYR"){
+                  for(key in $scope.datasets){
+                    var total = 0;
+                    for(var i = 0; i< $scope.datasets[key].length; i++)
+                    {
+                        //datasets[key][i].convertedAmount = res.data[j].rate[datasets[key][i].currency] *  datasets[key][i].amount;
+                        console.log(res.data[j].rate[$scope.datasets[key][i].currency]);
+                        if(typeof res.data[j].rate[$scope.datasets[key][i].currency] == "undefined")
+                           res.data[j].rate[$scope.datasets[key][i].currency] = 1;
+
+                        total = total + ($scope.datasets[key][i].amount *res.data[j].rate[$scope.datasets[key][i].currency]);
+                    }
+                    $scope.datasets[key]['total'] = parseFloat(total).toFixed(2);
+                  }
+              }
+            }
+        });
+
+    
+   
+    $scope.doRefresh = function(){
+
+       var expenses = $localstorage.get('expenses');
+       $scope.expenses = JSON.parse(expenses);
+        $scope.$broadcast('scroll.refreshComplete');
+
+    };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, $localstorage, $http) {
+   // $scope.chat = Chats.get($stateParams.chatId);
+   
+    $scope.Details = [];
+    $scope.title = $stateParams.key; 
+    $scope.total = 0;
+
+    var expenses = $localstorage.get('expenses');
+    expenses = JSON.parse(expenses);
+
+    $http.get('./models/currencies.json')
+       .then(function(res){
+            for(var i=0 ; i<res.data.length; i++)
+            {
+               if(res.data[i].label == "MYR"){
+                 for(var j=0 ; j<expenses.length; j++)
+                  {
+                     if(expenses[j].date == $stateParams.key){
+
+                       console.log(res.data[i].rate[expenses[j].currency]);
+                        if(typeof res.data[i].rate[expenses[j].currency] == "undefined")
+                           res.data[i].rate[expenses[j].currency] = 1;
+
+                       var rate = res.data[i].rate[expenses[j].currency];
+                       expenses[j].amount = expenses[j].amount * rate;
+                       $scope.Details.push(expenses[j]);
+                       var total = $scope.total + expenses[j].amount;
+                       $scope.total = total;
+                     }
+                  }
+               }
+            }
+        });
+})
+
+.controller('AccountCtrl', function($scope, $rootScope, $localstorage, $state, $http) {
+    //$scope.currencies=
+    console.log("flag");
+    $http.get('./models/currencies.json')
+       .then(function(res){
+          console.log(res.data);
+          $scope.currencies = res.data;
+
+           $rootScope.settings = { 
+            'enableFriends': true, 
+             'currency': $scope.currencies[1] 
+           };
+        });
+
+   
+
+
+    $scope.$on('$stateChangeStart', 
+    function(event, toState, toParams, fromState, fromParams){
+      if(fromState.name == "tab.account"){
+          console.log($rootScope.settings);
+          $localstorage.set('settings', JSON.stringify($rootScope.settings))
+
+       }
+    })
+
+    $scope.reset = function(){
+    	$localstorage.clear('expenses');
+    }
+
+})
+.controller('ChartCtrl', function($scope, $rootScope, $localstorage, $state, $http) {
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+});
---
+++ www/js/services.js
@@ -0,0 +1,98 @@
+angular.module('starter.services', [])
+.factory('_', function() {
+    return window._; // assumes underscore has already been loaded on the page
+  })
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    },
+    clear:function(key){
+      $window.localStorage[key]="";
+    }
+  }
+}])
+.factory('Categories',['$http' , function($http){  
+  return {
+    all:function (){
+      return $http.get('./models/category.json')
+       .then(function(res){
+         console.log(res);
+          //$scope.categories = chunk(res.data, 5);  
+          return res;
+        });
+    },
+    get:function(label){
+      return $http.get('./models/category.json')
+       .then(function(res){
+           for(var i = 0; i<res.data.length; i++)
+           {
+             if(res.data[i].label == label){
+               return res.data[i];
+             }
+           }
+        });
+
+      return null;
+    }
+
+  }
+  
+}])
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Andrew Jostlin',
+    lastText: 'Did you get the ice cream?',
+    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+  }, {
+    id: 3,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 4,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
